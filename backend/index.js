0843305511d49f4477e6802cebe44e748f26cac3
pritpan <pratikpandey196@gmail.com>
2025-08-17 14:57:06 +0530

    Update CORS origin URL to production domain for deployment

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: "twitter-clone-pratik.vercel.app", 
   credentials: true,
 };
 app.use(cors(corsOptions));
